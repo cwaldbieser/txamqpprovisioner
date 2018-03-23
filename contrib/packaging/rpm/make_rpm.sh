cac3db76118c1f537e2458763b8b0cd95a795a3c
root <root@grouper0.dev.lafayette.edu>
2018-03-23 12:18:06 -0400

    Added RPM build scripts.

---
+++ contrib/packaging/rpm/make_rpm.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+THISDIR="$( cd $(dirname $0); pwd)"
+SCRIPT="$THISDIR/$(basename $0)"
+ITERATION=${ITERATION:-1}
+FPM=${FPM:-/usr/bin/fpm}
+OS_DEPS='python27,libffi_local >= 6.0.2,jython >= 2.7.0'
+RPM_STEM="txamqpprovisioner"
+SOFTWARE=$(readlink -f "/opt/txamqpprovisioner")
+AFTER_INSTALL="$THISDIR/post-install.sh"
+OUTDIR=${OUTDIR:-/var/local/rpms}
+
+function get_version
+{
+    pushd "$WEBROOT" > /dev/null
+    git describe --abbrev=0 --tags
+    popd > /dev/null
+}
+VERSION=$(get_version)
+
+pushd "$OUTDIR" > /dev/null
+"$FPM" \
+    --verbose -s dir -t rpm -d "$OS_DEPS" -n "$RPM_STEM" \
+    -v "$VERSION" --iteration "$ITERATION" \
+    -x '*.pyc' -x '*.pem' -x '*.pid' -x '*.log' -x '*.gitignore' \
+    -x '*.cfg' -x '*.tgz' -x '*.zip' -x '*.tar.gz' -x '*.pkc' \
+    -x '*.log' \
+    --exclude '*/.git' \
+    --rpm-use-file-permissions \
+    --after-install "$AFTER_INSTALL" \
+    -p "$OUTDIR" \
+    "$SOFTWARE"
+popd > /dev/null
+
---
+++ contrib/packaging/rpm/post-install.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+# Application log folder.
+mkdir -p /var/log/txamqpprovisioners
+mkdir -p /etc/txamqpprovisioners
+mkdir -p /var/run/txampqprovisioners
