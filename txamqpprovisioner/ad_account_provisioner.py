3c42a44d88c4ed54af2bc6b439dbc072206668ff
Carl Waldbieser <waldbiec@lafayette.edu>
2018-05-10 16:43:29 -0400

    In progress ...
    Need to crreate random password on account creation.

--- txamqpprovisioner/ad_account_provisioner.py
+++ txamqpprovisioner/ad_account_provisioner.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 import datetime
 import json
+import collections
 import commentjson
 import jinja2 
 from textwrap import dedent
@@ -17,8 +18,9 @@ from ldaptor.protocols import (
     pureber,
     pureldap
 )
+import six
 from six import iteritems
-from twisted.internet import defer, task
+from twisted.internet import defer, ssl
 from twisted.internet.defer import (
     inlineCallbacks, 
     returnValue,
@@ -37,6 +39,9 @@ from interface import (
     IProvisioner,
 )
 from utils import get_plugin_factory
+
+def iterable_not_string(arg):
+    return isinstance(arg, collections.Iterable) and not isinstance(arg, six.string_types)
 
 def escape_quote(s):
     """
@@ -87,7 +92,7 @@ class LDAPClientManager(object):
         
 class ADAccountProvisionerFactory(object):
     implements(IPlugin, IProvisionerFactory)
-    tag = "override_this_tag"
+    tag = "ad_account_provisioner"
     opt_help = "AD Account Provisioner"
     opt_usage = "This plugin does not support any options."
 
@@ -110,6 +115,9 @@ class ADAccountProvisioner(object):
     search_filter = None
     account_template_path = None
     account_template_ = None
+    use_starttls = True
+    starttls_hostname = None
+    starttls_trust_anchor = None
 
     def load_config(self, config_file, default_log_level, logObserverFactory):
         """                                                             
@@ -132,6 +140,9 @@ class ADAccountProvisioner(object):
             try:
                 self.provision_group = config["provision_group"].lower()
                 self.endpoint_s = config["endpoint"]
+                self.use_starttls = bool(config.get("use_starttls", True))
+                self.starttls_hostname = config.get("starttls_hostname", "localhost")
+                starttls_trust_anchor = config.get('starttls_trust_anchor', None)
                 self.bind_dn = config["bind_dn"]
                 self.bind_passwd = config["bind_passwd"]
                 self.base_dn = config["base_dn"]
@@ -142,11 +153,27 @@ class ADAccountProvisioner(object):
                     "A required option was missing: '{}:{}'.".format(
                         section, ex.args[0]))
             self.parse_account_template_()
+            self.load_starttls_trust_anchor(starttls_trust_anchor)
         except Exception as ex:
             d = self.reactor.callLater(0, self.reactor.stop)
             log.failure("Provisioner failed to initialize: {}".format(ex))
             raise
         return defer.succeed(None)
+
+    def load_starttls_trust_anchor(self, pem_path):
+        """
+        Load the startTLS trust anchor from a file in PEM format.
+        """
+        if not self.use_starttls:
+            return
+        if pem_path is None:
+            return
+        starttls_hostname = self.starttls_hostname
+        assert starttls_hostname is not None, "Must set option `starttls_hostname` when using `starttls_trust_anchor`."
+        with open(pem_path, "r") as f:
+            data = f.read()
+        authority = ssl.Certificate.loadPEM(data)
+        self.starttls_trust_anchor = ssl.optionsForClientTLS(starttls_hostname, authority)
 
     def parse_account_template_(self):
         """
@@ -227,9 +254,12 @@ class ADAccountProvisioner(object):
         bind_passwd = self.bind_passwd
         base_dn = self.base_dn
         reactor = self.reactor
+        use_starttls = self.use_starttls
+        starttls_trust_anchor = self.starttls_trust_anchor
         e = clientFromString(reactor, endpoint_str)
         client = yield connectProtocol(e, ldapclient.LDAPClient())
-        yield client.startTLS()
+        if use_starttls:
+            yield client.startTLS(starttls_trust_anchor)
         yield client.bind(bind_dn, bind_passwd)
         defer.returnValue(client)
 
@@ -289,38 +319,75 @@ class ADAccountProvisioner(object):
                 attributes = attrib_map[subject]
                 account = self.compose_account_(subject, attritbutes)
                 dn = account['dn']
-                yield self.provision_subject_(account, client=c)
+                create_hint = (dn in dn_set)
+                yield self.provision_subject_(account, client=c, create_hint=create_hint)
                 dn_set.discard(dn)
             for dn in dn_set:
                 yield self.disable_dn_(dn, client=c) 
 
+    def entry_to_attribs_(entry):
+    """
+    Convert a simple mapping to the data structures required for an
+    entry in the DIT.
+
+    Returns: (dn, attributes)
+    """
+    attribs = {}
+    dn = None
+    for prop, value in entry.items():
+        if prop == 'dn':
+            dn = value.encode('utf-8')
+            continue
+        msg = (
+                "Account template value should be an iterable: "
+                "prop='{}', value='{}'"
+                ).format(prop, value)
+        assert iterable_not_string(value), msg 
+        attribs[prop] = set(value)
+    if dn is None:
+        raise Exception("Entry needs to include key, `dn`!")
+    ldap_attrs = []
+    for attrib, values in attribs.items():
+        ldap_attrib_type = pureldap.LDAPAttributeDescription(attrib)
+        l = []
+        for value in values:
+            if (isinstance(value, unicode)):
+                value = value.encode('utf-8')
+            l.append(pureldap.LDAPAttributeValue(value))
+        ldap_values = pureber.BERSet(l)
+        ldap_attrs.append((ldap_attrib_type, ldap_values))
+    return dn, ldap_attrs
+
     @inlineCallbacks
-    def provision_subject_(self, account, client=None):
+    def provision_subject_(self, account, client=None, create_hint=False):
         """
         Provision subject from internal account representation.
         """
         log = self.log
-        attribs = {}
-        dn = None
-        for attrib, value in account.items():
-            if attrib == 'dn':
-                dn = value
-                continue
-            attribs.setdefault(attrib, set([])).add(value)
-        if dn is None:
-            raise Exception("Template needs to include `dn`!")
-        ldap_attrs = []
-        for attrib, values in attribs.items():
-            ldap_attrib_type = pureldap.LDAPAttributeDescription(attrib)
-            l = []
-            for value in values:
-                if (isinstance(value, unicode)):
-                    value = value.encode('utf-8')
-                l.append(pureldap.LDAPAttributeValue(value))
-            ldap_values = pureber.BERSet(l)
-            ldap_attrs.append((ldap_attrib_type, ldap_values))
+        if create_hint:
+            strategies = [self.add_entry_, self.update_entry_]
+        else:
+            strategies = [self.update_entry_, self.add_entry_]
+        result_codes = []
+        success = False
+        for strategy in strategies:
+            result_code = yield strategy(account, client)
+            if result_code == 0:
+                returnValue(None)
+            result_codes.append(result_code)
+        log.debug("Couldn't create or update entry.")
+        log.debug("account: {}".format(account))
+        log.debug("result codes: {}".format(result_codes))
+        raise Exception("Could not create nor update entry.") 
+
+    @inlineCallbacks
+    def add_entry_(self, account, client):
+        """
+        Attempt to add an LDAP entry.
+        """
+        dn, attributes = entry_to_attribs_(account)
         op = pureldap.LDAPAddRequest(
-            entry=str(dn),
+            entry=dn,
             attributes=ldap_attrs)
         log.debug("LDAP ADD request: {add_req}", add_req=repr(op))
         unbind = False
@@ -330,6 +397,45 @@ class ADAccountProvisioner(object):
         with LDAPClientManager(client, active=unbind) as c:
             response = yield c.send(op)
         log.debug("LDAP ADD response: {add_resp}", add_resp=repr(response))
+        result_code = response.resultCode
+        allowed_results = (
+            ldaperrors.Success.resultCode, 
+            ldaperrors.entryAlreadyExists.resultCode)
+        if result_code not in allowed_results:
+            msg = response.errorMessage
+            raise Exception("Error adding entry: result_code={}, msg={}".format(
+                result_code, msg))
+        returnValue(result_code)
+
+    @inlineCallbacks
+    def update_entry_(self, account, client):
+        """
+        Attempt to update and LDAP entry.
+        """
+        attribs = [delta.Replace(prop, list(value)) 
+            for prop, value in account if prop != "dn"]  
+        try:
+            dn = ccount['dn']
+        except KeyError:
+            raise Exception("Account template must include a `dn` property.")
+        mod = delta.ModifyOp(dn, [attribs])
+        log.debug("LDAP MOD request: {mod_req}", mod_req=repr(mod))
+        unbind = False
+        if client is None:
+            client = yield self.get_ldap_client_()
+            unbind = True
+        with LDAPClientManager(client, active=unbind) as c:
+            response = yield c.send(mod)
+        log.debug("LDAP MOD response: {mod_resp}", mod_resp=repr(response))
+        result_code = response.resultCode
+        allowed_results = (
+            ldaperrors.Success.resultCode, 
+            ldaperrors.noSuchObject.resultCode)
+        if result_code not in allowed_results:
+            msg = response.errorMessage
+            raise Exception("Error adding entry: result_code={}, msg={}".format(
+                result_code, msg))
+        returnValue(result_code)
 
     @inlineCallbacks
     def provision_subject(self, subject, attributes):
