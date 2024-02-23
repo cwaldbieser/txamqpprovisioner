ec452842680860d7cc119b525bd9ab6f9602171b
Carl Waldbieser <cwaldbieser@gmail.com>
2024-02-23 16:40:04 -0500

    [chore] lint/format crashplan provisioner
    [chore] lint/format crashplan provisioner

--- txamqpprovisioner/crashplan_provisioner.py
+++ txamqpprovisioner/crashplan_provisioner.py
@@ -1,22 +1,16 @@
-
 from __future__ import print_function
+
 import itertools
 import json
 import random
 import string
-import urlparse
+
 import commentjson
-from rest_provisioner import (
-    APIResponseError,
-    OptionMissingError,
-    RESTProvisioner,
-    RESTProvisionerFactory, 
-    StringProducer,
-)
-from twisted.internet.defer import (
-    inlineCallbacks, 
-    returnValue,
-)
+import urlparse
+from rest_provisioner import (APIResponseError, OptionMissingError,
+                              RESTProvisioner, RESTProvisionerFactory,
+                              StringProducer)
+from twisted.internet.defer import inlineCallbacks, returnValue
 
 
 class CrashplanProvisioner(RESTProvisioner):
@@ -34,13 +28,14 @@ class CrashplanProvisioner(RESTProvisioner):
       should not be managed by the provisioner (e.g. a back door admin account).
       These accounts are identified by their match_values.
     """
+
     api_username = None
     page_size = 100
-    
+
     def get_match_value_from_remote_account(self, remote_account):
         """
         Given a remote account, `remote_account`, extract the
-        value that will be used to match the remote account 
+        value that will be used to match the remote account
         to the local subject.
         Returns None if a match value cannot be constructed for the remote
         account.
@@ -78,14 +73,12 @@ class CrashplanProvisioner(RESTProvisioner):
         config = self.config
         api_username = config.get("api_username", None)
         if api_username is None:
-            raise OptionMissingError(
-                "The `api_username` option is missing!") 
+            raise OptionMissingError("The `api_username` option is missing!")
         self.api_username = api_username
         self.page_size = config.get("page_size", 100)
         domain = config.get("domain", None)
         if domain is None:
-            raise OptionMissingError(
-                "The `domain` option is missing!") 
+            raise OptionMissingError("The `domain` option is missing!")
         self.domain = domain
 
     @inlineCallbacks
@@ -99,8 +92,8 @@ class CrashplanProvisioner(RESTProvisioner):
         domain = self.domain
         http_client = self.http_client
         headers = {
-            'Accept': ['application/json'],
-            'User-Agent': ['CrashPlan User Provisioner'],
+            "Accept": ["application/json"],
+            "User-Agent": ["CrashPlan User Provisioner"],
         }
         client_secret = self.client_secret
         params = {
@@ -112,25 +105,36 @@ class CrashplanProvisioner(RESTProvisioner):
         log.debug("method: GET, URL: {url}", url=auth_url)
         log.debug("auth: ({}, {}***)".format(basic_auth[0], basic_auth[1][:3]))
         response = yield http_client.get(
-            auth_url,
-            auth=basic_auth,
-            params=params,
-            headers=headers)
+            auth_url, auth=basic_auth, params=params, headers=headers
+        )
         resp_code = response.code
-        log.debug("API call to obtain token is complete.  Response code: {code}", code=resp_code)
+        log.debug(
+            "API call to obtain token is complete.  Response code: {code}",
+            code=resp_code,
+        )
         if resp_code == 200:
             try:
                 doc = yield response.json()
             except Exception as ex:
-                log.error("Error attempting to parse response to authentication request.")
+                log.error(
+                    "Error attempting to parse response to authentication request."
+                )
                 raise
             if not "data" in doc:
-                log.error("Error attempting to parse response to authentication request.")
-                raise Exception("Error parsing authentication response.  Missing element `data`.")
-            data = doc['data']
+                log.error(
+                    "Error attempting to parse response to authentication request."
+                )
+                raise Exception(
+                    "Error parsing authentication response.  Missing element `data`."
+                )
+            data = doc["data"]
             if not "v3_user_token" in data:
-                log.error("Error attempting to parse response to authentication request.")
-                raise Exception("Error parsing authentication response.  Missing element `v3_user_token`.")
+                log.error(
+                    "Error attempting to parse response to authentication request."
+                )
+                raise Exception(
+                    "Error parsing authentication response.  Missing element `v3_user_token`."
+                )
             self.auth_token = data["v3_user_token"]
             log.debug("New auth token obtained.")
         else:
@@ -138,14 +142,14 @@ class CrashplanProvisioner(RESTProvisioner):
             content = yield response.content()
             raise Exception(
                 "Unable to obtain valid auth token.  Response {}: {}".format(
-                resp_code,
-                content)
+                    resp_code, content
+                )
             )
 
     @inlineCallbacks
     def authorize_api_call(self, method, url, **http_options):
         """
-        Given the components of an *unauthenticated* HTTP client request, 
+        Given the components of an *unauthenticated* HTTP client request,
         return the components of an authenticated request.
 
         Should return a tuple of (method, url, http_options)
@@ -162,7 +166,7 @@ class CrashplanProvisioner(RESTProvisioner):
     @inlineCallbacks
     def get_all_api_ids_and_match_values(self):
         """
-        Load all the remote subject IDs and match values from the 
+        Load all the remote subject IDs and match values from the
         user accounts that exist on the remote sevice.
         Note: If a match value cannot be constructed for a remote
         account, it will not be included in the output of this function.
@@ -174,25 +178,21 @@ class CrashplanProvisioner(RESTProvisioner):
         url = "{}/api/User".format(prefix)
         page_size = self.page_size
         headers = {
-            'Accept': ['application/json'],
+            "Accept": ["application/json"],
         }
-        params = {
-            'pgSize': page_size
-        }
+        params = {"pgSize": page_size}
         identifiers = []
         pg_num = 0
         while True:
             pg_num += 1
-            params['pgNum'] = pg_num
+            params["pgNum"] = pg_num
             log.debug("URL (GET): {url}", url=url)
             log.debug("headers: {headers}", headers=headers)
             log.debug("params: {params}", params=params)
             try:
                 resp = yield self.make_authenticated_api_call(
-                    "GET",
-                    url,
-                    headers=headers,
-                    params=params)
+                    "GET", url, headers=headers, params=params
+                )
             except Exception as ex:
                 log.error("Error fetching all remote user data.")
                 raise
@@ -200,12 +200,16 @@ class CrashplanProvisioner(RESTProvisioner):
             data = parsed.get("data", None)
             if data is None:
                 log.error("Error attempting to parse response to get ALL users.")
-                raise Exception("Error attempting to parse response to get ALL users.  Missing `data` element.")
+                raise Exception(
+                    "Error attempting to parse response to get ALL users.  Missing `data` element."
+                )
             total_count = parsed.get("totalCount", -1)
             users = data.get("users", None)
             if users is None:
                 log.error("Error attempting to parse response to get ALL users.")
-                raise Exception("Error attempting to parse response to get ALL users.  Missing `users` element.")
+                raise Exception(
+                    "Error attempting to parse response to get ALL users.  Missing `users` element."
+                )
             for entry in users:
                 api_id = self.get_api_id_from_remote_account(entry)
                 match_value = self.get_match_value_from_remote_account(entry)
@@ -227,15 +231,12 @@ class CrashplanProvisioner(RESTProvisioner):
         prefix = self.url_prefix
         url = "{}/api/User/{}".format(prefix, api_id)
         headers = {
-            'Accept': ['application/json'],
+            "Accept": ["application/json"],
         }
         identifiers = []
         log.debug("URL (GET): {url}", url=url)
         log.debug("headers: {headers}", headers=headers)
-        resp = yield self.make_authenticated_api_call(
-            "GET",
-            url,
-            headers=headers)
+        resp = yield self.make_authenticated_api_call("GET", url, headers=headers)
         remote_account = yield resp.json()
         returnValue(remote_account)
 
@@ -249,8 +250,8 @@ class CrashplanProvisioner(RESTProvisioner):
         prefix = self.url_prefix
         url = "{}/console/api/UserDeactivation/{}".format(prefix, api_id)
         headers = {
-            'Accept': ['application/json'],
-            'Content-Type': ['application/json'],
+            "Accept": ["application/json"],
+            "Content-Type": ["application/json"],
         }
         if active:
             method = "DELETE"
@@ -259,10 +260,7 @@ class CrashplanProvisioner(RESTProvisioner):
         log.debug("url: {url}", url=url)
         log.debug("menthod: {method}", method=method)
         log.debug("headers: {headers}", headers=headers)
-        resp = yield self.make_authenticated_api_call(
-            method,
-            url,
-            headers=headers)
+        resp = yield self.make_authenticated_api_call(method, url, headers=headers)
         resp_code = resp.code
         try:
             content = yield resp.content()
@@ -280,7 +278,11 @@ class CrashplanProvisioner(RESTProvisioner):
         log.debug("entered api_deprovision_subject()")
         resp_code = yield self.change_subject_status_(api_id, active=False)
         if resp_code != 201:
-            raise Exception("API call to deprovision subject returned HTTP status {}".format(resp_code))
+            raise Exception(
+                "API call to deprovision subject returned HTTP status {}".format(
+                    resp_code
+                )
+            )
         returnValue(None)
 
     @inlineCallbacks
@@ -296,26 +298,30 @@ class CrashplanProvisioner(RESTProvisioner):
         local_match_value = self.get_match_value_from_local_subject(subject, attributes)
         url = "{}/api/User".format(prefix)
         headers = {
-            'Accept': ['application/json'],
+            "Accept": ["application/json"],
         }
         params = {
-            'username': local_match_value,
+            "username": local_match_value,
         }
         log.debug("Making API call to fetch account from local match value.")
-        log.debug("method: {method}, url: {url}, params: {params}",
+        log.debug(
+            "method: {method}, url: {url}, params: {params}",
             method="GET",
             url=url,
-            params=params)
+            params=params,
+        )
         resp = yield self.make_authenticated_api_call(
-            "GET",
-            url,
-            headers=headers,
-            params=params)
+            "GET", url, headers=headers, params=params
+        )
         resp_code = resp.code
         parsed = yield resp.json()
         log.debug("Result of fetch attempt: {parsed}", parsed=parsed)
         if resp_code not in (200,):
-            raise Exception("API call to fetch remote account ID returned HTTP status {}".format(resp_code))
+            raise Exception(
+                "API call to fetch remote account ID returned HTTP status {}".format(
+                    resp_code
+                )
+            )
         data = parsed.get("data", None)
         if data is None:
             log.debug("No `data` element in parsed response.")
@@ -340,36 +346,36 @@ class CrashplanProvisioner(RESTProvisioner):
         log.debug("entered api_update_subject()")
         resp_code = yield self.change_subject_status_(api_id, active=True)
         if resp_code != 204:
-            raise Exception("API call to activate subject returned HTTP status {}".format(resp_code))
+            raise Exception(
+                "API call to activate subject returned HTTP status {}".format(resp_code)
+            )
         prefix = self.url_prefix
         url = "{}/api/User/{}".format(prefix, api_id)
         headers = {
-            'Accept': ['application/json'],
-            'Content-Type': ['application/json'],
+            "Accept": ["application/json"],
+            "Content-Type": ["application/json"],
         }
         surname = attributes.get("sn", [""])[0]
         givenname = attributes.get("givenName", [""])[0]
         props = {
-            'firstName': givenname,
-            'lastName': surname,
+            "firstName": givenname,
+            "lastName": surname,
         }
         serialized = json.dumps(props)
-        body = StringProducer(serialized.encode('utf-8'))
+        body = StringProducer(serialized.encode("utf-8"))
         log.debug("url: {url}", url=url)
         log.debug("headers: {headers}", headers=headers)
         log.debug("body: {body}", body=serialized)
         resp = yield self.make_authenticated_api_call(
-            'PUT',  
-            url, 
-            data=body, 
-            headers=headers)
+            "PUT", url, data=body, headers=headers
+        )
         returnValue(resp)
 
     @inlineCallbacks
     def api_add_subject(self, subject, attributes):
         """
         Use the API to add subjects.
-        
+
         Returns the API ID of the newly created remote account or None.
         If None is returned, the API ID will not be cached and require
         a lookup on future use.
@@ -380,13 +386,16 @@ class CrashplanProvisioner(RESTProvisioner):
         if api_id:
             resp_code = yield self.change_subject_status_(api_id, active=True)
             if resp_code != 204:
-                raise Exception("API call to activate subject returned HTTP status {}".format(resp_code))
+                raise Exception(
+                    "API call to activate subject returned HTTP status {}".format(
+                        resp_code
+                    )
+                )
             returnValue(api_id)
+
 
 class CrashplanProvisionerFactory(RESTProvisionerFactory):
     tag = "crashplan_provisioner"
     opt_help = "Code42 CrashPlan API Provisioner"
     opt_usage = "This plugin does not support any options."
     provisioner_factory = CrashplanProvisioner
-
-
